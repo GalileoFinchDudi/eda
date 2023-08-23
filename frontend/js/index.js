0fe0f5873519c133022b155a63c067eed6340c0a
Galileo Finch <galileofinchdudi@gmail.com>
2023-08-23 16:07:00 +0800

    feat: 优化page2，增加轮播

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -82,4 +82,13 @@ var layout = {
   },
 };
 
+var mySwiper = new Swiper(".swiper", {
+  // loop: true, // 循环模式选项
+
+  // 如果需要分页器
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
 loader();
